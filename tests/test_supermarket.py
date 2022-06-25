43e70f095c8265f0526e4cc03856253576b75b5b
Matthias Fischer <3135771+MatthiasFischer90@users.noreply.github.com>
2022-06-25 13:11:14 +0200

    test: Fix test_ten_percent_discount to actually test discount
    Before this commit, test_ten_percent_discount would create a Discount
    for toothbrushes, but then only add apples to the cart. With this
    commit, toothbrushes are now added to the cart, so the test actually
    checks if the discount is applied.
    The way the discount prices are calculated and then checked in this test
    is problematic for a number of reasons, but this will be changed in future
    commits - this commit was only concerned with making sure that
    test_ten_percent_discount actually tests what it promises.

--- tests/test_supermarket.py
+++ tests/test_supermarket.py
@@ -19,14 +19,23 @@ def test_ten_percent_discount():
 
     cart = ShoppingCart()
     cart.add_item_quantity(apples, 2.5)
+    cart.add_item_quantity(toothbrush, 2)
 
     receipt = teller.checks_out_articles_from(cart)
 
-    assert 4.975 == pytest.approx(receipt.total_price(), 0.01)
-    assert [] == receipt.discounts
-    assert 1 == len(receipt.items)
-    receipt_item = receipt.items[0]
-    assert apples == receipt_item.product
-    assert 1.99 == receipt_item.price
-    assert 2.5 * 1.99 == pytest.approx(receipt_item.total_price, 0.01)
-    assert 2.5 == receipt_item.quantity
+    assert 6.75 == pytest.approx(receipt.total_price(), 0.01)
+    assert 2 == len(receipt.items)
+
+    # check apples
+    receipt_item_apples = receipt.items[0]
+    assert apples == receipt_item_apples.product
+    assert 1.99 == receipt_item_apples.price
+    assert 2.5 * 1.99 == pytest.approx(receipt_item_apples.total_price, 0.01)
+    assert 2.5 == receipt_item_apples.quantity
+
+    # check toothbrushes
+    receipt_item_toothbrush = receipt.items[1]
+    assert toothbrush == receipt_item_toothbrush.product
+    assert 0.99 == receipt_item_toothbrush.price
+    assert 2 * 0.99 == pytest.approx(receipt_item_toothbrush.total_price, 0.01)
+    assert 2 == receipt_item_toothbrush.quantity
