c32a5196e95d3e2631580755e9b66822a1200aed
Matthias Fischer <3135771+MatthiasFischer90@users.noreply.github.com>
2022-06-25 18:20:55 +0200

    refactor: Add leading underscore to methods in ReceiptPrinter
    All methods in ReceiptPrinter except print_receipt are just called by
    ReceiptPrinter itself and are not meant to be called externally. This is now
    denoted by adding a leading underscore to those methods.

--- receipt_printer.py
+++ receipt_printer.py
@@ -10,26 +10,28 @@ class ReceiptPrinter:
     def print_receipt(self, receipt: Receipt) -> str:
         result = ""
         for item in receipt.items:
-            receipt_item = self.print_receipt_item(item)
+            receipt_item = self._print_receipt_item(item)
             result += receipt_item
 
         for discount in receipt.discounts:
-            discount_presentation = self.print_discount(discount)
+            discount_presentation = self._print_discount(discount)
             result += discount_presentation
 
         result += "\n"
-        result += self.present_total(receipt)
+        result += self._present_total(receipt)
         return str(result)
 
-    def print_receipt_item(self, item: ReceiptItem) -> str:
-        total_price_printed = self.print_price(item.total_price)
+    def _print_receipt_item(self, item: ReceiptItem) -> str:
+        total_price_printed = self._print_price(item.total_price)
         name = item.product.name
-        line = self.format_line_with_whitespace(name, total_price_printed)
+        line = self._format_line_with_whitespace(name, total_price_printed)
         if item.quantity != 1:
-            line += f"  {self.print_price(item.price)} * {self.print_quantity(item)}\n"
+            line += (
+                f"  {self._print_price(item.price)} * {self._print_quantity(item)}\n"
+            )
         return line
 
-    def format_line_with_whitespace(self, name: str, value: str) -> str:
+    def _format_line_with_whitespace(self, name: str, value: str) -> str:
         line = name
         whitespace_size = self.columns - len(name) - len(value)
         for i in range(whitespace_size):
@@ -38,21 +40,21 @@ class ReceiptPrinter:
         line += "\n"
         return line
 
-    def print_price(self, price: float) -> str:
+    def _print_price(self, price: float) -> str:
         return "%.2f" % price
 
-    def print_quantity(self, item: ReceiptItem) -> str:
+    def _print_quantity(self, item: ReceiptItem) -> str:
         if ProductUnit.EACH == item.product.unit:
             return str(item.quantity)
         else:
             return "%.3f" % item.quantity
 
-    def print_discount(self, discount: Discount) -> str:
+    def _print_discount(self, discount: Discount) -> str:
         name = f"{discount.description} ({discount.product.name})"
-        value = self.print_price(discount.discount_amount)
-        return self.format_line_with_whitespace(name, value)
+        value = self._print_price(discount.discount_amount)
+        return self._format_line_with_whitespace(name, value)
 
-    def present_total(self, receipt: Receipt) -> str:
+    def _present_total(self, receipt: Receipt) -> str:
         name = "Total: "
-        value = self.print_price(receipt.total_price())
-        return self.format_line_with_whitespace(name, value)
+        value = self._print_price(receipt.total_price())
+        return self._format_line_with_whitespace(name, value)
